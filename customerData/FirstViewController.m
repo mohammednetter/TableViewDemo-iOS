4d4d2f3e4875959512d0ac1576dfdd13aa517f30
Admin <admin@Admins-Mac.local>
2014-10-29 05:03:30 +0300

    Initial Commit

---
+++ customerData/FirstViewController.m
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.m
+//  customerData
+//
+//  Created by Admin on 10/29/14.
+//  Copyright (c) 2014 Admin. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ customerData/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  customerData
+//
+//  Created by Admin on 10/29/14.
+//  Copyright (c) 2014 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
