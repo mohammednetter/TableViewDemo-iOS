aac3424d4ac5a6f666fed70d6810dd22acbfb979
mohammednetter <mohammed.netter@gmail.com>
2015-04-06 18:40:47 -0700

    first commit

---
+++ customerData/MNAddViewController.h
@@ -0,0 +1,13 @@
+//
+//  MNAddViewController.h
+//  customerData
+//
+//  Created by Admin on 10/29/14.
+//  Copyright (c) 2014 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MNAddViewController : UIViewController
+
+@end
---
+++ customerData/MNUpdateViewController.m
@@ -0,0 +1,37 @@
+//
+//  MNUpdateViewController.m
+//  customerData
+//
+//  Created by Admin on 10/29/14.
+//  Copyright (c) 2014 Admin. All rights reserved.
+//
+
+#import "MNUpdateViewController.h"
+
+@interface MNUpdateViewController ()
+
+@end
+
+@implementation MNUpdateViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
